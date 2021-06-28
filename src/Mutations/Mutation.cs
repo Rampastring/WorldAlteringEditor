89125461d9e303369fe9da1ab76bc061a29ba1b9
Rampastring <rampastring@outlook.com>
2021-06-29 01:52:20 +0300

    Add Mutation and MutationManager

---
+++ src/Mutations/Mutation.cs
@@ -0,0 +1,14 @@
+﻿namespace TSMapEditor.Mutations
+{
+    /// <summary>
+    /// A base class for all mutations.
+    /// A mutation modifies something in the map in a way that makes the effect
+    /// un-doable and re-doable through the Undo/Redo system.
+    /// </summary>
+    public abstract class Mutation
+    {
+        public abstract void Perform();
+
+        public abstract void Undo();
+    }
+}
---
+++ src/Mutations/MutationManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+
+namespace TSMapEditor.Mutations
+{
+    /// <summary>
+    /// The Undo / Redo system.
+    /// </summary>
+    public class MutationManager
+    {
+        public List<Mutation> UndoList { get; } = new List<Mutation>();
+        public List<Mutation> RedoList { get; } = new List<Mutation>();
+
+        /// <summary>
+        /// Performs a new mutation on the map.
+        /// </summary>
+        /// <param name="mutation">The mutation to perform.</param>
+        public void PerformMutation(Mutation mutation)
+        {
+            mutation.Perform();
+            RedoList.Clear();
+            UndoList.Add(mutation);
+        }
+
+        /// <summary>
+        /// Undoes the last mutation to the map.
+        /// </summary>
+        public void Undo()
+        {
+            if (UndoList.Count == 0)
+                return;
+
+            int lastUndoIndex = UndoList.Count - 1;
+            UndoList[lastUndoIndex].Undo();
+            RedoList.Add(UndoList[lastUndoIndex]);
+            UndoList.RemoveAt(lastUndoIndex);
+        }
+
+        /// <summary>
+        /// Redoes the last un-done mutation on the map.
+        /// </summary>
+        public void Redo()
+        {
+            if (RedoList.Count == 0)
+                return;
+
+            int lastRedoIndex = RedoList.Count - 1;
+            RedoList[lastRedoIndex].Perform();
+            UndoList.Add(RedoList[lastRedoIndex]);
+            RedoList.RemoveAt(lastRedoIndex);
+        }
+    }
+}
