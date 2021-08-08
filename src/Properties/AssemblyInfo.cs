a135ac08bedb4de94dfc31b66792fe3d05f60eec
Rampastring <rampastring@outlook.com>
2021-08-08 23:07:49 +0300

    Update AssemblyInfo

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -4,12 +4,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TSMapEditor")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Dawn of the Tiberium Age Scenario Editor")]
+[assembly: AssemblyDescription("Dawn of the Tiberium Age Scenario Editor")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TSMapEditor")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCompany("Rampastring")]
+[assembly: AssemblyProduct("Dawn of the Tiberium Age")]
+[assembly: AssemblyCopyright("Copyright © Rami 'Rampastring' Pasanen 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.6.6.6")]
+[assembly: AssemblyFileVersion("0.6.6.6")]
