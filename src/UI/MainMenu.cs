c549788bfda130ce5b4d9e9333e25cff2312d8d4
Rampastring <rampastring@outlook.com>
2021-12-19 14:27:13 +0200

    Implement file browser to main menu

--- src/UI/MainMenu.cs
+++ src/UI/MainMenu.cs
@@ -15,6 +15,8 @@ namespace TSMapEditor.UI
 {
     public class MainMenu : EditorPanel
     {
+        private const string DirectoryPrefix = "<DIR> ";
+
         public MainMenu(WindowManager windowManager) : base(windowManager)
         {
         }
@@ -24,12 +26,15 @@ namespace TSMapEditor.UI
         private EditorTextBox tbGameDirectory;
         private EditorTextBox tbMapPath;
         private EditorButton btnLoad;
+        private EditorListBox lbFileList;
         private int loadingStage;
+
+        string fileListDirectoryPath = string.Empty;
 
         public override void Initialize()
         {
             Name = nameof(MainMenu);
-            Width = 350;
+            Width = 370;
 
             var lblGameDirectory = new XNALabel(WindowManager);
             lblGameDirectory.Name = nameof(lblGameDirectory);
@@ -44,35 +49,143 @@ namespace TSMapEditor.UI
             tbGameDirectory.Y = lblGameDirectory.Bottom + Constants.UIVerticalSpacing;
             tbGameDirectory.Width = Width - Constants.UIEmptySideSpace * 2;
             tbGameDirectory.Text = UserSettings.Instance.GameDirectory;
+            tbGameDirectory.TextChanged += TbGameDirectory_TextChanged;
             AddChild(tbGameDirectory);
 
-            var lblDescription = new XNALabel(WindowManager);
-            lblDescription.Name = nameof(lblDescription);
-            lblDescription.X = Constants.UIEmptySideSpace;
-            lblDescription.Y = tbGameDirectory.Bottom + Constants.UIEmptyTopSpace;
-            lblDescription.Text = "Path of the map file to load (relative to game directory):";
-            AddChild(lblDescription);
+            var lblMapPath = new XNALabel(WindowManager);
+            lblMapPath.Name = nameof(lblMapPath);
+            lblMapPath.X = Constants.UIEmptySideSpace;
+            lblMapPath.Y = tbGameDirectory.Bottom + Constants.UIEmptyTopSpace;
+            lblMapPath.Text = "Path of the map file to load (can be relative to game directory):";
+            AddChild(lblMapPath);
 
             tbMapPath = new EditorTextBox(WindowManager);
             tbMapPath.Name = nameof(tbMapPath);
             tbMapPath.X = Constants.UIEmptySideSpace;
-            tbMapPath.Y = lblDescription.Bottom + Constants.UIVerticalSpacing;
+            tbMapPath.Y = lblMapPath.Bottom + Constants.UIVerticalSpacing;
             tbMapPath.Width = Width - Constants.UIEmptySideSpace * 2;
             tbMapPath.Text = UserSettings.Instance.LastScenarioPath;
             AddChild(tbMapPath);
+
+            var lblDirectoryListing = new XNALabel(WindowManager);
+            lblDirectoryListing.Name = nameof(lblDirectoryListing);
+            lblDirectoryListing.X = Constants.UIEmptySideSpace;
+            lblDirectoryListing.Y = tbMapPath.Bottom + Constants.UIVerticalSpacing * 2;
+            lblDirectoryListing.Text = "Alternatively, select a map file below:";
+            AddChild(lblDirectoryListing);
+
+            lbFileList = new EditorListBox(WindowManager);
+            lbFileList.Name = nameof(lbFileList);
+            lbFileList.X = Constants.UIEmptySideSpace;
+            lbFileList.Y = lblDirectoryListing.Bottom + Constants.UIVerticalSpacing;
+            lbFileList.Width = Width - Constants.UIEmptySideSpace * 2;
+            lbFileList.Height = 300;
+            lbFileList.SelectedIndexChanged += LbFileList_SelectedIndexChanged;
+            lbFileList.DoubleLeftClick += LbFileList_DoubleLeftClick;
+            AddChild(lbFileList);
 
             btnLoad = new EditorButton(WindowManager);
             btnLoad.Name = nameof(btnLoad);
             btnLoad.Width = 100;
             btnLoad.Text = "Load";
-            btnLoad.Y = tbMapPath.Bottom + Constants.UIEmptyTopSpace;
+            btnLoad.Y = lbFileList.Bottom + Constants.UIEmptyTopSpace;
             AddChild(btnLoad);
             btnLoad.CenterOnParentHorizontally();
             btnLoad.LeftClick += BtnLoad_LeftClick;
 
             Height = btnLoad.Bottom + Constants.UIEmptyBottomSpace;
 
+
+            if (Path.IsPathRooted(tbMapPath.Text))
+            {
+                fileListDirectoryPath = Path.GetDirectoryName(tbMapPath.Text);
+            }
+            else
+            {
+                fileListDirectoryPath = Path.GetDirectoryName(tbGameDirectory.Text + tbMapPath.Text);
+            }
+
+            fileListDirectoryPath = fileListDirectoryPath.Replace('/', Path.DirectorySeparatorChar).Replace('\\', Path.DirectorySeparatorChar);
+
+            ListFiles();
+
             base.Initialize();
+        }
+
+        private void LbFileList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lbFileList.SelectedItem == null)
+                return;
+
+            if (lbFileList.SelectedItem.Tag != null)
+                return;
+
+            // Select file
+            if (!fileListDirectoryPath.EndsWith(Path.DirectorySeparatorChar.ToString()))
+                fileListDirectoryPath += Path.DirectorySeparatorChar;
+
+            tbMapPath.Text = fileListDirectoryPath + lbFileList.SelectedItem.Text;
+        }
+
+        private void TbGameDirectory_TextChanged(object sender, EventArgs e)
+        {
+            fileListDirectoryPath = tbGameDirectory.Text;
+            ListFiles();
+        }
+
+        private void LbFileList_DoubleLeftClick(object sender, EventArgs e)
+        {
+            if (lbFileList.SelectedItem == null)
+                return;
+
+            if (lbFileList.SelectedIndex == 0)
+            {
+                // Special case -- go up a directory
+                fileListDirectoryPath = Path.GetDirectoryName(fileListDirectoryPath.TrimEnd('/', '\\'));
+                ListFiles();
+                return;
+            }
+
+            if (!fileListDirectoryPath.EndsWith(Path.DirectorySeparatorChar.ToString()))
+                fileListDirectoryPath += Path.DirectorySeparatorChar;
+
+            if (lbFileList.SelectedItem.Tag != null)
+            {
+                // Browse to next directory
+                fileListDirectoryPath = fileListDirectoryPath + lbFileList.SelectedItem.Text.Substring(DirectoryPrefix.Length) + Path.DirectorySeparatorChar;
+                ListFiles();
+                return;
+            }
+
+            // Select file
+            tbMapPath.Text = fileListDirectoryPath + lbFileList.SelectedItem.Text;
+            BtnLoad_LeftClick(this, EventArgs.Empty);
+        }
+
+        private void ListFiles()
+        {
+            lbFileList.SelectedIndex = -1;
+            lbFileList.Clear();
+
+            if (string.IsNullOrWhiteSpace(fileListDirectoryPath) || !Directory.Exists(fileListDirectoryPath))
+            {
+                return;
+            }
+
+            lbFileList.AddItem(new XNAListBoxItem(".. <Directory Up>", Color.Gray) { Tag = new object() });
+
+            var directories = Directory.GetDirectories(fileListDirectoryPath);
+            foreach (string dir in directories)
+            {
+                string dirName = dir.Substring(dir.LastIndexOf(Path.DirectorySeparatorChar) + 1);
+                lbFileList.AddItem(new XNAListBoxItem(DirectoryPrefix + dirName, Color.LightGray) { Tag = new object() }); // Yay for wasting memory
+            }
+
+            var files = Directory.GetFiles(fileListDirectoryPath);
+            foreach (string file in files)
+            {
+                lbFileList.AddItem(Path.GetFileName(file));
+            }
         }
 
         private void BtnLoad_LeftClick(object sender, EventArgs e)
@@ -93,7 +206,11 @@ namespace TSMapEditor.UI
 
             UserSettings.Instance.GameDirectory.UserDefinedValue = gameDirectory;
 
-            if (!File.Exists(Path.Combine(gameDirectory, tbMapPath.Text)))
+            string mapPath = Path.Combine(gameDirectory, tbMapPath.Text);
+            if (Path.IsPathRooted(tbMapPath.Text))
+                mapPath = tbMapPath.Text;
+
+            if (!File.Exists(mapPath))
             {
                 EditorMessageBox.Show(WindowManager,
                     "Invalid map path",
@@ -134,6 +251,8 @@ namespace TSMapEditor.UI
             IniFile firestormIni = new IniFile(Path.Combine(gameDirectory, "INI/Enhance.ini"));
             IniFile artIni = new IniFile(Path.Combine(gameDirectory, "INI/Art.ini"));
             IniFile artFSIni = new IniFile(Path.Combine(gameDirectory, "INI/ArtE.INI"));
+            IniFile fsaIni = new IniFile(Path.Combine(gameDirectory, "INI/FSA.INI"));
+            IniFile.ConsolidateIniFiles(artFSIni, fsaIni);
             IniFile mapIni = new IniFile(Path.Combine(gameDirectory, mapPath));
             //IniFile mapIni = new IniFile(Path.Combine(GameDirectory, "Maps/Default/a_buoyant_city.map"));
             Map map = new Map();
