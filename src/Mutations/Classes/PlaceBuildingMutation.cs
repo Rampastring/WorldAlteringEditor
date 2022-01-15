354c3ea9ddc5d1d90e7b79911abf9f12b7c1c139
Rampastring <rampastring@outlook.com>
2022-01-15 14:58:05 +0200

    Fix crash when placing a building on a cell that has a vehicle

--- src/Mutations/Classes/PlaceBuildingMutation.cs
+++ src/Mutations/Classes/PlaceBuildingMutation.cs
@@ -26,8 +26,8 @@ namespace TSMapEditor.Mutations.Classes
         {
             var cell = MutationTarget.Map.GetTileOrFail(cellCoords);
 
-            if (cell.Vehicle != null)
-                throw new InvalidOperationException(nameof(PlaceVehicleMutation) + ": the cell already has a vehicle!");
+            if (cell.Structure != null)
+                throw new InvalidOperationException(nameof(PlaceBuildingMutation) + ": the cell already has a building!");
 
             var structure = new Structure(buildingType);
             structure.Owner = MutationTarget.ObjectOwner;
