c487ebc2f77ea8082116a5136c1e33b818f1cf7f
Rampastring <rampastring@outlook.com>
2022-03-29 01:03:45 +0300

    Add icon

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Rampastring")]
 [assembly: AssemblyProduct("Dawn of the Tiberium Age")]
-[assembly: AssemblyCopyright("Copyright © Rami 'Rampastring' Pasanen 2021")]
+[assembly: AssemblyCopyright("Copyright © Rami 'Rampastring' Pasanen 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,4 +31,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.7.*")]
+[assembly: AssemblyVersion("0.8.*")]
