745584675da42ec1569019abc33e9441059c7281
Rampastring <rampastring@outlook.com>
2022-09-14 01:50:32 +0300

    Update deletion mode cursor action to support camera zoom

--- src/TSMapEditor/UI/CursorActions/DeletionModeAction.cs
+++ src/TSMapEditor/UI/CursorActions/DeletionModeAction.cs
@@ -17,6 +17,7 @@ namespace TSMapEditor.UI.CursorActions
         public override void DrawPreview(Point2D cellCoords, Point2D cameraTopLeftPoint)
         {
             Point2D cellTopLeftPoint = CellMath.CellTopLeftPointFromCellCoords(cellCoords, CursorActionTarget.Map.Size.X) - cameraTopLeftPoint;
+            cellTopLeftPoint = cellTopLeftPoint.ScaleBy(CursorActionTarget.Camera.ZoomLevel);
 
             const string text = "Delete";
             var textDimensions = Renderer.GetTextDimensions(text, Constants.UIBoldFont);
