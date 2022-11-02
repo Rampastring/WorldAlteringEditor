477a556b66ebf4aac15f77275574e90d4ee5f85f
Rampastring <rampastring@outlook.com>
2022-11-02 22:21:34 +0200

    Fix YDrawOffset of smudges

--- src/TSMapEditor/Models/Smudge.cs
+++ src/TSMapEditor/Models/Smudge.cs
@@ -5,5 +5,10 @@
         public override RTTIType WhatAmI() => RTTIType.Smudge;
 
         public SmudgeType SmudgeType { get; set; }
+
+        public override int GetYDrawOffset()
+        {
+            return Constants.CellSizeY / -2;
+        }
     }
 }
