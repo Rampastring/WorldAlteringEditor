b9b574c1c856bed3ecbeef8c5871bc790407e20b
Rampastring <rampastring@outlook.com>
2023-03-15 21:22:37 +0200

    Default StandingFrames to value of WalkFrames

--- src/TSMapEditor/Models/ArtConfig/VehicleArtConfig.cs
+++ src/TSMapEditor/Models/ArtConfig/VehicleArtConfig.cs
@@ -7,10 +7,12 @@ namespace TSMapEditor.Models.ArtConfig
         public bool Voxel { get; set; }
         public bool Remapable { get; set; }
         public int StartStandFrame { get; set; } = -1;
-        public int StandingFrames { get; set; } = 1;
+        public int StandingFrames { get; set; }
         public int StartWalkFrame { get; set; } = -1;
         public int WalkFrames { get; set; } = 1;
         public int Facings { get; set; } = 1;
+
+        public int FiringFrames { get; set; } = -1;
 
         /// <summary>
         /// Vinifera addition, override for start turret frame.
@@ -26,9 +28,9 @@ namespace TSMapEditor.Models.ArtConfig
             Voxel = iniSection.GetBooleanValue(nameof(Voxel), Voxel);
             Remapable = iniSection.GetBooleanValue(nameof(Remapable), Remapable);
             StartStandFrame = iniSection.GetIntValue(nameof(StartStandFrame), StartStandFrame);
-            StandingFrames = iniSection.GetIntValue(nameof(StandingFrames), StandingFrames);
             StartWalkFrame = iniSection.GetIntValue(nameof(StartWalkFrame), StartWalkFrame);
             WalkFrames = iniSection.GetIntValue(nameof(WalkFrames), WalkFrames);
+            StandingFrames = iniSection.GetIntValue(nameof(StandingFrames), WalkFrames); // intentionally defaults to walkframes, the game does that too
             Facings = iniSection.GetIntValue(nameof(Facings), Facings);
             StartTurretFrame = iniSection.GetIntValue(nameof(StartTurretFrame), StartTurretFrame);
         }
