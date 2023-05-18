6e3981c3cd9b6d0983dc8305e35ef53249dd6711
Rampastring <rampastring@outlook.com>
2023-05-18 23:44:31 +0300

    Fix tunnel placement cursor to support cell height

--- src/TSMapEditor/UI/CursorActions/PlaceTubeCursorAction.cs
+++ src/TSMapEditor/UI/CursorActions/PlaceTubeCursorAction.cs
@@ -34,7 +34,7 @@ namespace TSMapEditor.UI.CursorActions
 
         public override void DrawPreview(Point2D cellCoords, Point2D cameraTopLeftPoint)
         {
-            Point2D cellTopLeftPoint = CellMath.CellTopLeftPointFromCellCoords(cellCoords, CursorActionTarget.Map) - cameraTopLeftPoint;
+            Point2D cellTopLeftPoint = CellMath.CellTopLeftPointFromCellCoords_3D(cellCoords, CursorActionTarget.Map) - cameraTopLeftPoint;
 
             cellTopLeftPoint = cellTopLeftPoint.ScaleBy(CursorActionTarget.Camera.ZoomLevel);
 
