8dfbf1580e5a0b90f222a857ca79eff95b807d10
Rampastring <rampastring@outlook.com>
2023-09-23 00:44:56 +0300

    Fix Change Techno Owner cursor action to support 2D mode

--- src/TSMapEditor/UI/CursorActions/ChangeTechnoOwnerAction.cs
+++ src/TSMapEditor/UI/CursorActions/ChangeTechnoOwnerAction.cs
@@ -24,7 +24,10 @@ namespace TSMapEditor.UI.CursorActions
 
             MapTile tile = CursorActionTarget.Map.GetTile(cellCoords);
             Point2D cellTopLeftPoint = CellMath.CellTopLeftPointFromCellCoords(cellCoords, CursorActionTarget.Map) - cameraTopLeftPoint;
-            cellTopLeftPoint.Y -= tile.Level * Constants.CellHeight;
+
+            if (!Is2DMode)
+                cellTopLeftPoint.Y -= tile.Level * Constants.CellHeight;
+
             cellTopLeftPoint = cellTopLeftPoint.ScaleBy(CursorActionTarget.Camera.ZoomLevel);
 
             Renderer.DrawTexture(
