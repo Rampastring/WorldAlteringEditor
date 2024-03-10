53f9824c829098a76b83301707f1481dca8686bf
Rampastring <rampastring@outlook.com>
2024-03-10 12:20:05 +0200

    Support comma in tileset name for terrain generator presets

--- src/TSMapEditor/Mutations/Classes/TerrainGenerationMutation.cs
+++ src/TSMapEditor/Mutations/Classes/TerrainGenerationMutation.cs
@@ -224,6 +224,8 @@ namespace TSMapEditor.Mutations.Classes
 
     public class TerrainGeneratorTileGroup
     {
+        private const string CommaReplacement = "{comma}";
+
         public TerrainGeneratorTileGroup(TileSet tileSet, List<int> tileIndicesInSet, double openChance, double overlapChance)
         {
             TileSet = tileSet ?? throw new ArgumentNullException(nameof(tileSet));
@@ -239,7 +241,7 @@ namespace TSMapEditor.Mutations.Classes
 
         public string GetConfigString()
         {
-            string config = $"{OpenChance},{OverlapChance},{TileSet.SetName}";
+            string config = $"{OpenChance},{OverlapChance},{TileSet.SetName.Replace(",", CommaReplacement)}";
             if (TileIndicesInSet != null && TileIndicesInSet.Count > 0)
             {
                 config += "," + string.Join(",", TileIndicesInSet);
@@ -257,7 +259,13 @@ namespace TSMapEditor.Mutations.Classes
 
             double openChance = Conversions.DoubleFromString(parts[0], 0.0);
             double overlapChance = Conversions.DoubleFromString(parts[1], 0.0);
-            var tileSet = allTileSets.Find(ts => ts.AllowToPlace && ts.LoadedTileCount > 0 && ts.SetName == parts[2]);
+
+            string tileSetName = parts[2];
+            // Hack to support comma in tileset name, YR unfortunately has one in an important LAT
+            // and unlike objects, tilesets have no ININame that we could use instead
+            tileSetName = parts[2].Replace(CommaReplacement, ",");
+
+            var tileSet = allTileSets.Find(ts => ts.AllowToPlace && ts.LoadedTileCount > 0 && ts.SetName == tileSetName);
             if (tileSet == null)
                 return null;
 
