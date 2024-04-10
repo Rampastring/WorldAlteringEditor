6a47b5d121d1546db73723848c16f6230afeaaae
Rampastring <rampastring@outlook.com>
2024-04-10 16:51:45 +0300

    Fix crash when hovering cursor over cells with invalid sub tile index data

--- src/TSMapEditor/UI/TileInfoDisplay.cs
+++ src/TSMapEditor/UI/TileInfoDisplay.cs
@@ -93,7 +93,7 @@ namespace TSMapEditor.UI
             textRenderer.AddTextLine(new XNATextPart("TileSet: ", Constants.UIDefaultFont, subtleTextColor));
             textRenderer.AddTextPart(new XNATextPart(tileSet.SetName + " (" + tileGraphics.TileSetId + ")", Constants.UIDefaultFont, baseTextColor));
 
-            MGTMPImage subCellImage = tileGraphics.TMPImages[MapTile.SubTileIndex];
+            MGTMPImage subCellImage = MapTile.SubTileIndex < tileGraphics.TMPImages.Length ? tileGraphics.TMPImages[MapTile.SubTileIndex] : null;
             string terrainType = subCellImage != null && subCellImage.TmpImage != null ? Helpers.LandTypeToString(subCellImage.TmpImage.TerrainType) : "Unknown";
 
             textRenderer.AddTextLine(new XNATextPart("Terrain Type: ", Constants.UIDefaultFont, subtleTextColor));
